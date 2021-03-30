faa91d4d5089dfad1f3945fe7f357034594e54f7
wangyang <35348462@qq.com>
2021-03-30 16:22:35 +0800

    gin中间件zap viper

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 /*
 https://www.liwenzhou.com/page/8/
 https://www.bilibili.com/video/BV16E411H7og?p=22&spm_id_from=pageDriver
+https://github.com/Q1mi/goweb_pro
 */
 func main() {
 	fmt.Println("hello world")
