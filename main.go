5f08e50c89495611314cb0eb44e16d5216e08ad1
wangyang <35348462@qq.com>
2021-04-01 11:03:51 +0800

    gin中间件zap viper

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import "fmt"
 https://www.liwenzhou.com/page/8/
 https://www.bilibili.com/video/BV16E411H7og?p=22&spm_id_from=pageDriver
 https://github.com/Q1mi/goweb_pro
+https://gitee.com/moxi159753/LearningNotes/tree/master/Golang/Gin%E6%A1%86%E6%9E%B6/
+源码地址：https://github.com/Q1mi/bubble
 */
 func main() {
 	fmt.Println("hello world")
